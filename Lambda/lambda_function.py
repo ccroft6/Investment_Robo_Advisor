ebdad915eab7d470c2d2a1dac38195ef46935a59
Catherine <91442380+ccroft6@users.noreply.github.com>
2022-04-11 15:17:14 -0700

    Add validate_data function

--- Lambda/lambda_function.py
+++ Lambda/lambda_function.py
@@ -26,7 +26,6 @@ def build_validation_result(is_valid, violated_slot, message_content):
         "message": {"contentType": "PlainText", "content": message_content},
     }
 
-
 ### Dialog Actions Helper Functions ###
 def get_slots(intent_request):
     """
@@ -79,37 +78,33 @@ def close(session_attributes, fulfillment_state, message):
 
     return response
 
-
-"""
-Step 3: Enhance the Robo Advisor with an Amazon Lambda Function
-
-In this section, you will create an Amazon Lambda function that will validate the data provided by the user on the Robo Advisor.
-
-1. Start by creating a new Lambda function from scratch and name it `recommendPortfolio`. Select Python 3.7 as runtime.
-
-2. In the Lambda function code editor, continue by deleting the AWS generated default lines of code, then paste in the starter code provided in `lambda_function.py`.
-
-3. Complete the `recommend_portfolio()` function by adding these validation rules:
-
-    * The `age` should be greater than zero and less than 65.
-    * The `investment_amount` should be equal to or greater than 5000.
-
-4. Once the intent is fulfilled, the bot should respond with an investment recommendation based on the selected risk level as follows:
-
-    * **none:** "100% bonds (AGG), 0% equities (SPY)"
-    * **low:** "60% bonds (AGG), 40% equities (SPY)"
-    * **medium:** "40% bonds (AGG), 60% equities (SPY)"
-    * **high:** "20% bonds (AGG), 80% equities (SPY)"
-
-> **Hint:** Be creative while coding your solution, you can have all the code on the `recommend_portfolio()` function, or you can split the functionality across different functions, put your Python coding skills in action!
-
-5. Once you finish coding your Lambda function, test it using the sample test events provided for this Challenge.
-
-6. After successfully testing your code, open the Amazon Lex Console and navigate to the `recommendPortfolio` bot configuration, integrate your new Lambda function by selecting it in the “Lambda initialization and validation” and “Fulfillment” sections.
-
-7. Build your bot, and test it with valid and invalid data for the slots.
-
-"""
+def validate_data(age, investment_amount, intent_request):
+    """
+    Validates the data provided by the user.
+    """
+    
+    # Validate that the user's age is greater than zero and less than 65.
+    if age is not None:
+        age = parse_int(age)
+        if age < 0 or age > 65:
+            return build_validation_result(
+                False,
+                "age",
+                "You need to be between 1-64 years old to use this service. Please provide a valid age.",
+            )
+    
+    # Validate the investment amount, it should be >= 5000
+    if investment_amount is not None:
+        investment_amount = parse_int(investment_amount) 
+        if investment_amount < 5000:
+            return build_validation_result(
+                False, 
+                "investmentAmount",
+                "The minimum investment amount is $5000. Please provide a valid investment amount so I can recommend an appropriate portfolio.",
+            )
+    
+    # A True results is returned if age or amount are valid
+    return build_validation_result(True, None, None)
 
 
 ### Intents Handlers ###
@@ -117,15 +112,74 @@ def recommend_portfolio(intent_request):
     """
     Performs dialog management and fulfillment for recommending a portfolio.
     """
-
+    
+    # Gets slots' values 
     first_name = get_slots(intent_request)["firstName"]
     age = get_slots(intent_request)["age"]
     investment_amount = get_slots(intent_request)["investmentAmount"]
     risk_level = get_slots(intent_request)["riskLevel"]
+
+    
+    # Gets the invocation source, for Lex dialogs "DialogCodeHook" is expected.
     source = intent_request["invocationSource"]
 
-    # YOUR CODE GOES HERE!
+    if source == "DialogCodeHook":
+        # This code performs basic validation on the supplied input slots
+        
+        # Gets all the slots
+        slots = get_slots(intent_request)
+        
+        # Validates user's input using the validate_data function
+        validation_result = validate_data(age, investment_amount, intent_request)
+        
+        # If the data provided by the user is not valid, 
+        # the elicitSlot dialog action is used to re-prompt for the first violation detected
+        if not validation_result["isValid"]:
+            slots[validation_result["violatedSlot"]] = None # Cleans invalid slot 
+            # Returns an elicitSlot dialog to request new data for the invalid slot
+            return elicit_slot(
+                intent_request["sessionAttributes"],
+                intent_request["currentIntent"]["name"],
+                slots,
+                validation_result["violatedSlot"],
+                validation_result["message"],
+            )
+            
+        # Fetch current session attributes
+        output_session_attributes = intent_request["sessionAttributes"]
+        
+        # Once all slots are valid, a delegate dialog is returned to Lex to choose the next course of action
+        return delegate(output_session_attributes, get_slots(intent_request))
+    
+    # Initialize recommendation variable
+    risk_portfolio = ""
+    
+    # Indicate how to select the portfolio
+    if risk_level == "None":
+        risk_portfolio = "100% bonds (AGG), 0% equities (SPY)"
+    
+    elif risk_level == "Low":
+        risk_portfolio = "60% bonds (AGG), 40% equities (SPY)"
+    
+    elif risk_level == "Medium": 
+        risk_portfolio = "40% bonds (AGG), 60% equities (SPY)"
+    
+    elif risk_level == "High":
+        risk_portfolio = "20% bonds (AGG), 80% equities (SPY)"
 
+    # Return a message with the conversation's result
+    return close(
+        intent_request["sessionAttributes"],
+        "Fulfilled",
+        {
+            "contentType": "PlainText",
+            "content": """Thank you for your information, {}.
+            We recommend the following investment portfolio: {}.
+            """.format(
+                first_name, risk_portfolio
+            ),
+        },
+    )
 
 ### Intents Dispatcher ###
 def dispatch(intent_request):
